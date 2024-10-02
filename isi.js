7d9c6b7f17c2e7e7d4cfdbf3e297e27a578ea72a
zaays <irawanrezaa123@gmail.com>
2024-10-02 14:26:56 +0700

    Update isi.js

--- isi.js
+++ isi.js
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-      <title>Reza Dwi</title>
-  </head>
-  <body>
- <script language="JavaScript">
-  document.write("Belajar Pemrograman Javascript!");
-</script>
-  
-  <script language="JavaScript" src="isi.js"></script>
-  </body>
-  </html>
-
 document.writeln("<center>");
 document.writeln("<hr width=600 color=Black>");
 document.writeln("<h1>UNIVERSITAS LAMPUNG</H1>");
