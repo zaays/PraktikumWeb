d5a6e3e0c07631a3cc8387a1e8c946fb2d97888d
zaays <irawanrezaa123@gmail.com>
2024-10-02 14:19:22 +0700

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <title>Reza Dwi</title>
+  </head>
+  <body>
+ <script language="JavaScript">
+  document.write("Belajar Pemrograman Javascript!");
+</script>
+  
+  <script language="JavaScript" src="isi.js"></script>
+  </body>
+  </html>
+
+document.writeln("<center>");
+document.writeln("<hr width=600 color=Black>");
+document.writeln("<h1>UNIVERSITAS LAMPUNG</H1>");
+document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI<H2>");
+document.writeln("<h3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM<H3>");
+document.writeln("<hr width=600 color=Black>");
+document.writeln("</center>");
